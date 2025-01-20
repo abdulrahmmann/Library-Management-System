89483315221b2ec25826cc6eef85f73dea265d48
Abdulrahman <abd3lra7man.078@gmail.com>
2025-01-20 15:14:24 +0300

    initial comment, adding images, colors, fonts and tailwindcss

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'signin',
+    loadComponent: () =>
+      import('../app/auth/signin/signin.component').then((mod) => mod.SigninComponent)
+  },
+  {
+    path: 'signup',
+    loadComponent: () =>
+      import('../app/auth/signup/signup.component').then((mod) => mod.SignupComponent)
+  },
+];
